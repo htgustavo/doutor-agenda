f85efbef7ccc90f06d279be5329856f8c37e9fcd
Hilton <htn.contato@hotmail.com>
2025-05-27 11:40:54 -0300

    feat: add login page

---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+import SignInForm from "./components/sign-in-form"
+import SignUpForm from "./components/sign-up-form"
+
+const AuthenticationPage = () => {
+
+  return ( 
+    <div className="h-screen w-screen flex items-center justify-center">
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="register">Criar conta</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <SignInForm />
+        </TabsContent>
+        <TabsContent value="register">
+          <SignUpForm />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
+ 
+export default AuthenticationPage;

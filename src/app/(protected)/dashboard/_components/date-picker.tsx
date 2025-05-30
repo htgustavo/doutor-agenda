56e615dbecc5164574c8e30de8a18b20d6967fe9
Hilton <htn.contato@hotmail.com>
2025-05-30 12:05:13 -0300

    feat: add dashboard components

--- src/app/(protected)/dashboard/_components/date-picker.tsx
+++ src/app/(protected)/dashboard/_components/date-picker.tsx
@@ -1,45 +1,48 @@
-"use client"
+"use client";
 
-import { addMonths } from "date-fns"
-import { ptBR } from "date-fns/locale"
-import dayjs from "dayjs"
-import { CalendarIcon } from "lucide-react"
-import { parseAsIsoDate, useQueryState } from "nuqs"
-import * as React from "react"
-import { DateRange } from "react-day-picker"
+import { addMonths, format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { parseAsIsoDate, useQueryState } from "nuqs";
+import * as React from "react";
+import { DateRange } from "react-day-picker";
 
-import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
-import { cn } from "@/lib/utils"
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
 
 export function DatePicker({
   className,
 }: React.HTMLAttributes<HTMLDivElement>) {
-
-  const [from, setFrom] = useQueryState("from", parseAsIsoDate.withDefault(new Date()))
-  
-  const [to, setTo] = useQueryState("to", parseAsIsoDate.withDefault(addMonths(new Date(), 1)))
-
+  const [from, setFrom] = useQueryState(
+    "from",
+    parseAsIsoDate.withDefault(new Date()),
+  );
+  const [to, setTo] = useQueryState(
+    "to",
+    parseAsIsoDate.withDefault(addMonths(new Date(), 1)),
+  );
+  const handleDateSelect = (dateRange: DateRange | undefined) => {
+    if (dateRange?.from) {
+      setFrom(dateRange.from, {
+        shallow: false,
+      });
+    }
+    if (dateRange?.to) {
+      setTo(dateRange.to, {
+        shallow: false,
+      });
+    }
+  };
   const date = {
     from,
-    to
-  }
-
-  const handleDateSelect = (dateRange: DateRange | undefined) => {
-    if(dateRange?.from) {
-      setFrom(dateRange.from)
-    }
-
-    if(dateRange?.to) {
-      setTo(dateRange.to)
-    }
-  }
-
+    to,
+  };
   return (
     <div className={cn("grid gap-2", className)}>
       <Popover>
@@ -49,18 +52,23 @@ export function DatePicker({
             variant={"outline"}
             className={cn(
               "justify-start text-left font-normal",
-              !date && "text-muted-foreground"
+              !date && "text-muted-foreground",
             )}
           >
             <CalendarIcon />
             {date?.from ? (
               date.to ? (
                 <>
-                  {dayjs(date.from).format("LLL dd, y")} -{" "}
-                  {dayjs(date.to).format("LLL dd, y")}
+                  {format(date.from, "LLL dd, y", {
+                    locale: ptBR,
+                  })}{" "}
+                  -{" "}
+                  {format(date.to, "LLL dd, y", {
+                    locale: ptBR,
+                  })}
                 </>
               ) : (
-                dayjs(date.from).format("LLL dd, y")
+                format(date.from, "LLL dd, y")
               )
             ) : (
               <span>Pick a date</span>
@@ -80,5 +88,5 @@ export function DatePicker({
         </PopoverContent>
       </Popover>
     </div>
-  )
+  );
 }

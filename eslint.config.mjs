1f35fad704eb091869c9ea661adae72a7edfa4c2
Hilton <htn.contato@hotmail.com>
2025-06-02 20:46:52 -0300

    fix: removed simple import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import { FlatCompat } from "@eslint/eslintrc";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
@@ -12,15 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    plugins: {
-      "simple-import-sort": simpleImportSort,
-    },
-    rules: {
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
-    },
-  },
 ];
 
 export default eslintConfig;
